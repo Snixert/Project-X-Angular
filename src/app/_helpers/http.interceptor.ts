7a620c9ca37ec93731dd074020c2019827e37a59
Niller <niemeyer@live.dk>
2022-11-28 13:04:27 +0100

    Added Http Interceptor

--- src/app/_helpers/http.interceptor.ts
+++ src/app/_helpers/http.interceptor.ts
@@ -1,1 +1,23 @@
+// This is part of the implementation of storing a JSON Web Token (JWT) in a HttpOnly Cookie
+
 // intercepts and handles an HttpRequest or HttpResponse
+import { Injectable } from "@angular/core";
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+// intercept() gets an HttpRequest object, changes it and forwards it to HttpHandler object's handle() method. It transforms the HttpRequest object into an Observable<HttpEvents>
+// next: HttpHandler object, represents the next interceptor in the chain of interceptors. The final 'next' in the chain is the Angular HttpClient
+@Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        req = req.clone({
+            withCredentials: true,
+        });
+
+        return next.handle(req);
+    }
+}
+
+export const HttpInterceptorProviders = [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
+];
