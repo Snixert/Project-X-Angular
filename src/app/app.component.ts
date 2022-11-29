a0745637e95a8e322e027515d9e1e2903c9717cd
Niller <niemeyer@live.dk>
2022-11-29 08:44:28 +0100

    Configured root app component and navbar view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
+// Root component for the App, this defines the root tag <app-root></app-root> used in the index.html file.
+
 import { Component } from '@angular/core';
+import { StorageService } from './_services/storage.service';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
   title = 'project-x';
+
+  constructor(private storageService: StorageService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    // check if user is logged in, if true then get users roles and set value for showAdminBoard and showModeratorBoard flag. This controls how the template navbar displays items
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  // logout method that reloads the window
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+
+        window.location.reload();
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
