43533dc2ab5086e05e879d152e64063a6d6d867e
SBPi0 <sylvainbp@hotmail.com>
2022-12-02 11:21:11 +0100

    WorkInProgress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'shop',component:ShopComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ShopComponent,HomeComponent]
