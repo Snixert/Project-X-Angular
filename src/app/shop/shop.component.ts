7de4316cb794c999baee1107974e88090f4476c7
SBPi0 <sylvainbp@hotmail.com>
2022-12-05 14:52:42 +0100

    UI Done - Typescript in progress

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShopComponent implements OnInit {
+    htmlCards:string= "";
+    playerInfo:string= "";
+    playerCurrency:string="";
+   constructor() {
+    let text: string = "";
+    let text2: string = "";
+    let text3: string = "";
 
-  constructor() { }
+    let player = {
+      name: 'Cowkilla23',
+      currency: 4250
+    }
+    
+    let items = [
+      {
+        name: 'Big Axe',
+        price: 250,
+        picture: "assets/bigaxe_512x512.jpg"
+      },
+      {
+        name: 'Legendary Sword',
+        price: 5000,
+        picture: "assets/sword_512x512.jpg"
+      },
+      {
+        name: 'Stick',
+        price: 5,
+        picture: "assets/stick_512x512.png"
+      },
+      {
+        name: 'Shovel',
+        price: 45,
+        picture: "assets/shovel_512x512.jpeg"
+      },
+      {
+        name: 'Rock',
+        price: 2,
+        picture: "assets/rock_512x512.jpg"
+      }];
+      
+      text2 = `<span>`+player.name+`</span>
+      <img class="profilePic" src="assets/huey.jpg" alt="N/A">`
+
+      text3 = `<span>`+player.currency+`</span>`;
+      
+      items.forEach(function(item){
+      if(player.currency >= item.price){
+        text +=
+        `
+          <div class="card">
+            <img class="cardcash" src="assets/coins.png" alt="N/A">
+            <span class="price">`+item.price+`</span>
+            <p class="itemName">`+item.name+`</p>
+            <div class="buybutton">
+              <p class="buttonText">Buy</p>
+            </div>
+            <div>
+              <img class = "item" src="`+ item.picture+`" alt="N/A">
+            </div>
+          </div>
+        `
+      }
+      else{
+        text +=
+        `
+          <div class="card">
+            <img class="cardcash" src="assets/coins.png" alt="N/A">
+            <span class="price">`+item.price+`</span>
+            <p class="itemName">`+item.name+`</p>
+            <div class="cantbuybutton">
+              <p class="buttonText">Buy</p>
+            </div>
+            <div>
+              <img class = "item" src="`+ item.picture+`" alt="N/A">
+            </div>
+          </div>
+        `
+      }
+      });
+      this.htmlCards = text;
+      this.playerInfo = text2;
+      this.playerCurrency = text3;
+  }
 
   ngOnInit(): void {
   }
-
 }
