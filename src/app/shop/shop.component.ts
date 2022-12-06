60fe507e4c6efe6aca4ddf7b83d08d8ed4a1d8c5
SBPi0 <sylvainbp@hotmail.com>
2022-12-06 14:55:24 +0100

    progress on item cards

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
@@ -7,17 +7,42 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ShopComponent implements OnInit {
-    htmlCards:string= "";
-    playerInfo:string= "";
-    playerCurrency:string="";
+    htmlCards:string = "";
+    playerInfo:string;
+    playerCurrency:string;
+
+    items = [
+      {
+        name: 'Big Axe',
+        price: 250,
+        picture: "assets/bigaxe_512x512.jpg"
+      },
+      {
+        name: 'Legendary Sword',
+        price: 5000,
+        picture: "assets/sword_512x512.jpg"
+      },
+      {
+        name: 'Stick',
+        price: 5,
+        picture: "assets/stick_512x512.png"
+      },
+      {
+        name: 'Shovel',
+        price: 45,
+        picture: "assets/shovel_512x512.jpeg"
+      },
+      {
+        name: 'Rock',
+        price: 2,
+        picture: "assets/rock_512x512.jpg"
+      }];
+
    constructor() {
     let text: string = "";
-    let text2: string = "";
-    let text3: string = "";
-
     let player = {
       name: 'Cowkilla23',
-      currency: 4250
+      currency: 5100
     }
     
     let items = [
@@ -47,50 +72,63 @@ export class ShopComponent implements OnInit {
         picture: "assets/rock_512x512.jpg"
       }];
       
-      text2 = `<span>`+player.name+`</span>
+      this.playerInfo = `<span>`+player.name+`</span>
       <img class="profilePic" src="assets/huey.jpg" alt="N/A">`
 
-      text3 = `<span>`+player.currency+`</span>`;
+      this.playerCurrency = `<span>`+player.currency+`</span>`;
       
-      items.forEach(function(item){
-      if(player.currency >= item.price){
-        text +=
-        `
-          <div class="card">
-            <img class="cardcash" src="assets/coins.png" alt="N/A">
-            <span class="price">`+item.price+`</span>
-            <p class="itemName">`+item.name+`</p>
-            <div class="buybutton">
-              <p class="buttonText">Buy</p>
-            </div>
-            <div>
-              <img class = "item" src="`+ item.picture+`" alt="N/A">
-            </div>
-          </div>
-        `
-      }
-      else{
-        text +=
-        `
-          <div class="card">
-            <img class="cardcash" src="assets/coins.png" alt="N/A">
-            <span class="price">`+item.price+`</span>
-            <p class="itemName">`+item.name+`</p>
-            <div class="cantbuybutton">
-              <p class="buttonText">Buy</p>
-            </div>
-            <div>
-              <img class = "item" src="`+ item.picture+`" alt="N/A">
-            </div>
-          </div>
-        `
-      }
-      });
-      this.htmlCards = text;
-      this.playerInfo = text2;
-      this.playerCurrency = text3;
+      // items.forEach(function(item){
+      // if(player.currency >= item.price){
+      //   text +=
+      //   `
+      //     <div class="card">
+      //       <img class="cardcash" src="assets/coins.png" alt="N/A">
+      //       <span class="price">`+item.price+`</span>
+      //       <p class="itemName">`+item.name+`</p>
+      //       <div class="buybutton" id="buybutton">
+      //         <p class="buttonText">Buy</p>
+      //       </div>
+      //       <div>
+      //         <img class = "item" src="`+ item.picture+`" alt="N/A">
+      //       </div>
+      //     </div>
+      //   `
+      // }
+      // else{
+      //   text +=
+      //   `
+      //     <div class="card">
+      //       <img class="cardcash" src="assets/coins.png" alt="N/A">
+      //       <span class="price">`+item.price+`</span>
+      //       <p class="itemName">`+item.name+`</p>
+      //       <div class="cantbuybutton">
+      //         <p class="buttonText">Buy</p>
+      //       </div>
+      //       <div>
+      //         <img class = "item" src="`+ item.picture+`" alt="N/A">
+      //       </div>
+      //     </div>
+      //   `
+      // }
+      // });
+      // this.htmlCards = text;
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){    
+    // var buttns = document.getElementsByClassName("buybutton");
+    // for(var i = 0; i < buttns.length; i++){
+    //   buttns[i].addEventListener("click",(event)=>console.log(buttns[i].parentElement));
+    // }
+
+    // console.log(buttns[3].parentElement);
+
+  }
+
+  response(){
+    console.log("works");
+    console.log()
   }
 }
