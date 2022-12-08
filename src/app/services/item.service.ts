9b25bdabaeb21f23bb19229ea78560ee85066b5e
Jimmy Wolthers Nielsen <jwn@praktikcenter.lan>
2022-12-08 08:46:57 +0100

    import httpclient and add get methods for ItemService

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  baseApiUrl: string = environment.baseApiUrl;
+  constructor(private http: HttpClient) { }
+
+  getItems(): Observable<Item[]>{
+    return this.http.get<Item[]>(this.baseApiUrl + 'api/items');
+  }
+
+  getItem(id: number): Observable<Item>{
+    return this.http.get<Item>(this.baseApiUrl + 'api/items/' + id);
+  }
 }
