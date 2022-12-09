03d04b8c1b858deea5a2efa0b9947dba94d255f9
Jimmy Wolthers Nielsen <jwn@praktikcenter.lan>
2022-12-09 08:25:11 +0100

    add getPlayerWeapon to PlayerService

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
+import { Item } from '../models/item';
 import { Player } from '../models/player';
 
 @Injectable({
@@ -18,5 +19,9 @@ export class PlayerService {
 
   getPlayer(id: number): Observable<Player>{
     return this.http.get<Player>(this.baseApiUrl + 'api/players/' + id);
+  }
+
+  getPlayerWeapon(id: number): Observable<Item>{
+    return this.http.get<Item>(this.baseApiUrl + 'api/players/' + id + '/weapon');
   }
 }
