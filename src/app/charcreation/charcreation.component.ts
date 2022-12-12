c288dece5b035a89cd02a18cdfae590440680131
SBPi0 <sylvainbp@hotmail.com>
2022-12-12 14:06:44 +0100

    implemented checkbox check

--- src/app/charcreation/charcreation.component.ts
+++ src/app/charcreation/charcreation.component.ts
@@ -7,14 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharcreationComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+  image: string = "";
+  checkboxes = [{
+    id:1,image:'assets/huey.jpg',isChecked: false
+  },
+  {
+    id:2,image:'assets/huey.jpg',isChecked: false
+  },
+  {
+    id:3,image:'assets/huey.jpg',isChecked: false
+  }
+  ]
+
+  isChecked(userInput:number){
+    this.checkboxes.forEach((checkbox)=>{
+      if(userInput === checkbox.id){
+        checkbox.isChecked = true;
+        this.image = checkbox.image;
+      }
+      else{
+        checkbox.isChecked = false;
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
+    //needs to store info into player object
+    //then API post call
     const input = <HTMLInputElement>document.getElementById('textbox');
     console.log(input.value);
-    input.value = "";
+    console.log(this.image);
   }
 }
