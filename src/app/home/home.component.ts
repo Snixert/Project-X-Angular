e533339642996b9d37fa8ffce6185dcaf2d0b5fc
SBPi0 <sylvainbp@hotmail.com>
2022-12-14 14:54:01 +0100

    homepages wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PlayerService } from '../services/player.service';
+import { Player } from '../models/player';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private playerService: PlayerService) {
+  }
+  
+  //Need API call to check if logged in user has a player
+  account = {
+    playerId: 2
+  }
 
   ngOnInit(): void {
   }
-
 }
