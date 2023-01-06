d5d1023cd6e2b2ea100eff806ec4fc951d260677
Niller <niemeyer@live.dk>
2023-01-06 12:47:15 +0100

    Added auth guard

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,15 +1,37 @@
+/* Auth Guard is an angular route guard that is used to prevent unauthenticated users
+from accessing restricted routes. It does this by implementing the CanActivate interface
+which allows the guard to decide if a route can be activated with the canActivate()
+method. If the method returns 'true' the route is activated (allowed to proceed), if 
+the method returns 'false' the route is blocked.
+
+The Auth Guard uses the account service to check if the user is logged in. If they are
+it returns 'true' from the canActivate() method, otherwise it returns 'false' and redirects
+the user to the login page along with the 'returnUrl' in the query parameters.
+
+Angular route guards are attached to routes in the router config. This auth guard is used
+in app-routing.module.ts to protect the home page from '/users' routes.
+*/
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private router: Router, private accountService: AccountService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const user = this.accountService.userValue;
+    if (user) {
+      // authorized so return true
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+    return false;
   }
-  
+
 }
