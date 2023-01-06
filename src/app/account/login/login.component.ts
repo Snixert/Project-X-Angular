ff60b8cebc75324f87fb02b79cf6c6f9654c254e
Niller <niemeyer@live.dk>
2023-01-06 14:04:14 +0100

    WIP adding login components and layout

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,15 +1,70 @@
+/* The login component uses the account service to login to the application on
+form submit. It creates the form fields and validators using 'FormBuilder' to
+create an instance of a 'FormGroup' that is stored in the 'form' property. The
+'form' is then bound to the '<form>' element in the login component template
+using the '[formGroup]' directive.
+
+The component contains a convenience getter property 'f' to make it a bit easier
+to acces form controls. For example, you can access the password field in the
+template using 'f.password' instead of 'form.controls.password'.
+*/
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
+import { AccountService } from 'src/app/_services/account.service';
+import { AlertService } from 'src/app/_services/alert.service';
+
+@Component({ templateUrl: './login.component.html' })
 export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  // convenience getter for easy acces to the form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.accountService.login(this.f['username'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          // get return url from query parameters or default to home page
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+        },
+        error: error => {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      });
+  }
 }
